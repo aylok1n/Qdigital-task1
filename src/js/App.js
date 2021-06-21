65c16dfd4656080c680f806de12a131af3027208
aylok1n <nikolya.blog@gmail.com>
2021-06-21 16:48:34 +0400

    Delete browserHistory

--- src/js/App.js
+++ src/js/App.js
@@ -1,21 +1,20 @@
 import '../scss/index.scss'
 import React from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom'
-import { createBrowserHistory } from "history";
+
 import Home from './pages/Home'
 import Slider from './pages/Slider'
 import Error404 from './pages/Error404';
 
 class App extends React.Component {
   render() {
-  const history = createBrowserHistory();
   return (
     <Router >
     <div className="App">
       <Switch>
-        <Route history={history} exact path='/' component={Home}/>
-        <Route history={history} path='/slider' component={Slider}/>
-        <Route history={history} path='/error' component={Error404}/>
+        <Route exact path='/' component={Home}/>
+        <Route path='/slider' component={Slider}/>
+        <Route path='/error' component={Error404}/>
         <Redirect to='/error'/>
       </Switch>
     </div>
