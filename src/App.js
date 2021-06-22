6201b65a8965b5b7a4426b1c61077d96e29def57
aylok1n <nikolya.blog@gmail.com>
2021-06-22 16:01:06 +0400

    Fixed imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Text, View, TouchableOpacity, BackHandler, Linking} from 'react-native';
-import {NativeRouter, Route, Link} from 'react-router-native';
-import {createStore, compose, applyMiddleware} from 'redux';
-import {Provider} from 'react-redux';
-import {styles} from './styles/styles';
-import {reducer} from './js/redux/reducer';
+import { Text, View, TouchableOpacity, BackHandler, Linking } from 'react-native';
+import { NativeRouter, Link } from 'react-router-native';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { styles } from './styles/styles';
+import { reducer } from './js/redux/reducer';
 import thunk from 'redux-thunk';
-import {routes} from './js/routes';
+import { routes } from './js/routes';
 
 const store = createStore(reducer, compose(applyMiddleware(thunk)));
 
@@ -16,37 +16,33 @@ class App extends React.Component {
       <Provider store={store}>
         <NativeRouter>
           <View style={styles.App}>
+            {' '}
             {routes}
-
             <View style={styles.nav}>
               <Link to="/" underlayColor="#f0f4f7" style={styles.navItem}>
-                <Text>Home</Text>
+                <Text> Home </Text>{' '}
               </Link>
-
               <Link to="/slider" underlayColor="#f0f4f7" style={styles.navItem}>
-                <Text>Slider</Text>
+                <Text> Slider </Text>{' '}
               </Link>
-
               <Link to="/player" underlayColor="#f0f4f7" style={styles.navItem}>
-                <Text>Player</Text>
+                <Text> Player </Text>{' '}
               </Link>
-
               <TouchableOpacity
                 underlayColor="#f0f4f7"
                 style={styles.navItem}
                 onPress={() => Linking.openURL('https://q-digital.org')}>
-                <Text>Browser</Text>
+                <Text> Browser </Text>{' '}
               </TouchableOpacity>
-
               <TouchableOpacity
                 underlayColor="#f0f4f7"
                 style={styles.navItem}
                 onPress={BackHandler.exitApp}>
-                <Text>Exit</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </NativeRouter>
+                <Text> Exit </Text>{' '}
+              </TouchableOpacity>{' '}
+            </View>{' '}
+          </View>{' '}
+        </NativeRouter>{' '}
       </Provider>
     );
   }
